742e4b8bc42f73f7c6eaa4513752c49fee0e6f98
coenen <coenen@aliyun.com>
2019-09-11 11:34:51 +0800

    项目初始化

---
+++ Swift5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5
+//
+//  Created by coenen on 2019/9/11.
+//  Copyright © 2019 coenen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Swift5Tests/Swift5Tests.swift
@@ -0,0 +1,34 @@
+//
+//  Swift5Tests.swift
+//  Swift5Tests
+//
+//  Created by coenen on 2019/9/11.
+//  Copyright © 2019 coenen. All rights reserved.
+//
+
+import XCTest
+@testable import Swift5
+
+class Swift5Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
